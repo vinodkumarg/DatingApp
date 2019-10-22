9bf93ed534496661d34908c67904fe69e731b03f
Vinod <acervinod@gmail.com>
2019-10-22 23:13:31 +0530

    Initial Commit

---
+++ DatingApp.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly DataContext _context;
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public WeatherForecastController(DataContext context)
+        {
+            this._context = context;
+        }
+        // public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        // {
+        //     _logger = logger;
+        // }
+
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+            // var rng = new Random();
+            // return new string[]{"Value1","Value3"};
+            //     return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            //     {
+            //         Date = DateTime.Now.AddDays(index),
+            //         TemperatureC = rng.Next(-20, 55),
+            //         Summary = Summaries[rng.Next(Summaries.Length)]
+            //     })
+            //     .ToArray();
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(v => v.Id == id);
+            return Ok(value);
+        }
+    }
+}
